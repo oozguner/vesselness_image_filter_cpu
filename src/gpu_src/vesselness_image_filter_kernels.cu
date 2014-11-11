3268d5b2d873f6943c14972869295dccba56841f
Russell Jackson <RCJ@case>
2014-11-10 21:44:03 -0500

    RCJ, made the kernels object file compile into a code library

--- src/gpu_src/vesselness_image_filter_kernels.cu
+++ src/gpu_src/vesselness_image_filter_kernels.cu
@@ -1,8 +1,44 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014 Case Western Reserve University
+ *    Russell C Jackson <rcj33@case.edu>
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Case Western Reserve University, nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 
-#include "thinSegmentationGPU.h"
-//#include <opencv2/gpu/stream_accessor.hpp> 
-//#include "thinSegmentationKernels.cuh"
+
+#include <vesselness_image_filter_kernels.h>
+
 
 //This file defines the kernel functions used by the thin segmentation cuda code.
 
@@ -181,7 +217,7 @@ __global__ void gaussAngBlur(const PtrStepSz<float3> srcMat,PtrStepSz<float3> ds
 
 
 //image conversion functions
-
+/*
 void convertSegmentImagesGPU(const stereoImage& segIn,stereoImage& dispOut){
 
 	for(int lr = 0; lr < 2; lr++)
@@ -273,7 +309,7 @@ void segmentThinGPU(const Mat&src,Mat &dst,const segmentThinParam &inputParams)
 	cBuffer.create(floatSrc1.size(),tempGPU_XX.size()); 
 	/*gpu::convolve(floatSrc1,tempGPU_XX,cXX1,true,cBuffer); //,false,cBuffer);
 	gpu::convolve(floatSrc1,tempGPU_YY,cYY1,true,cBuffer); //,false,cBuffer);
-	gpu::convolve(floatSrc1,tempGPU_XY,cXY1,true,cBuffer); //,false,cBuffer); */
+	gpu::convolve(floatSrc1,tempGPU_XY,cXY1,true,cBuffer); //,false,cBuffer); * /
 
 	Mat tempCPU_XX,tempCPU_XY,tempCPU_YY;
 
@@ -431,7 +467,7 @@ void segmentThinGPU(const Mat&src,Mat &dst,const segmentThinParam &inputParams)
 		float errorM = abs(dstR[1]-vMag);
 		if(errorM > 0.001)
 			printf("Large diff\n");* /
-	} */
+	} * /
 
 #ifdef GPUOUTPUT
 	//tempD2= dst(Roi2);
@@ -440,7 +476,7 @@ void segmentThinGPU(const Mat&src,Mat &dst,const segmentThinParam &inputParams)
 
 
 } 
-
+*/
 
 
 
